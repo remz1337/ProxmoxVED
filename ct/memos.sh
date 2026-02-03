219eabdc55a8655029bd1e82239d7bc397130dc2
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2026-02-03 08:05:31 +0100

    Update memos.sh

--- ct/memos.sh
+++ ct/memos.sh
@@ -37,7 +37,7 @@ function update_script() {
     msg_info "Building Memos (patience)"
     cd /opt/memos/web
     $STD pnpm install --frozen-lockfile
-    $STD pnpm build
+    $STD pnpm release
     cd /opt/memos
     $STD go build -o memos ./cmd/memos
     msg_ok "Built Memos"
